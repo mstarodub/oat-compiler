7ba15dfac5338b9c9ef1da0dbf762b458fd188ad
Maxim Starodub <smaxim@student.ethz.ch>
2020-10-12 21:28:56 +0200

    wire up the tests someone else wrote

--- hw2/studenttests.ml
+++ hw2/studenttests.ml
@@ -1,13 +1,158 @@
 open Assert
 open X86
 open Simulator
+open Asm
 
 (* You can use this file for additional test cases to help your *)
 (* implementation.                                              *)
 
 
-let provided_tests : suite = [
-  Test ("Debug", [
-  ]);
+(* let provided_tests : suite = [ *)
+  (* Test ("Debug", [ *)
+  (* ]); *)
+(*  *)
+(* ] *)
 
-] 
+let shr =
+Gradedtests.test_machine
+[
+InsB0 (Movq, [ ~$16; ~%Rax ]);
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsB0 (Movq, [ ~$16; Gradedtests.stack_offset 0L ]);
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsB0 (Movq, [ ~$(-16); Gradedtests.stack_offset (-8L) ]);
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsB0 (Movq, [ ~$3; ~%Rcx ]);
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsB0 (Shrq, [ ~$1; ~%Rax ]);
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsB0 (Shrq, [ ~%Rcx; Gradedtests.stack_offset 0L ]);
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsB0 (Shrq, [ ~%Rcx; Gradedtests.stack_offset (-8L) ]);
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+]
+
+let sar =
+Gradedtests.test_machine
+[
+InsB0 (Movq, [ ~$16; ~%Rax ]);
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsB0 (Movq, [ ~$16; Gradedtests.stack_offset 0L ]);
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsB0 (Movq, [ ~$(-16); Gradedtests.stack_offset (-8L) ]);
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsB0 (Movq, [ ~$3; ~%Rcx ]);
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsB0 (Sarq, [ ~$1; ~%Rax ]);
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsB0 (Sarq, [ ~%Rcx; Gradedtests.stack_offset 0L ]);
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsB0 (Sarq, [ ~%Rcx; Gradedtests.stack_offset (-8L) ]);
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+InsFrag;
+]
+
+
+
+let bit_manipulation =
+[
+( "shr",
+Gradedtests.machine_test "rax=8 *65528=2 *65520=0x2000000000000000" 7 shr
+(fun m -> m.regs.(rind Rax) = 8L
+&& int64_of_sbytes (Gradedtests.sbyte_list m.mem (mem_size - 8)) = 2L
+&& int64_of_sbytes (Gradedtests.sbyte_list m.mem (mem_size - 16))
+= 2305843009213693950L) );
+( "sar",
+Gradedtests.machine_test "rax=8 *65528=2 *65520=-2" 7 sar (fun m -> m.regs.(rind Rax) = 8L
+&& int64_of_sbytes (Gradedtests.sbyte_list m.mem (mem_size - 8)) = 2L
+&& int64_of_sbytes (Gradedtests.sbyte_list m.mem (mem_size - 16))
+= -2L) );
+]
+
+
+let provided_tests : suite = [ Test ("student tests - Bit manipulation", bit_manipulation) ]
+
+
