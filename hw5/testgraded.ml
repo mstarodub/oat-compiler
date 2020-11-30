efb4d691cea468cff5b7d68c32d22e2b8cbcaf42
Maxim Starodub <smaxim@student.ethz.ch>
2020-11-30 10:56:51 +0100

    gradedtests: finalise

--- hw5/testgraded.ml
+++ hw5/testgraded.ml
@@ -1,26 +1,41 @@
 open Assert
 open Ast
 
-(* TODO: change these into actual, more complicated tests, e.g. permute the fields *)
-
 let unit_ok = Test ("gradedtest unit",
-  [ "typ_structex", (fun () ->
-    let c = Tctxt.add_struct Tctxt.empty "a" [{fieldName="x";ftyp=TInt}] in
-    let c = Tctxt.add_struct c "b" [{fieldName="x";ftyp=TInt}] in
+  [ "sub_substruct", (fun () ->
+    let c = Tctxt.add_struct Tctxt.empty "a" [
+        {fieldName="x";ftyp=
+          TRef (RFun ([TNullRef RString], RetVal (TNullRef (RArray TInt))))
+        }
+      ; {fieldName="y";ftyp=TNullRef (RStruct "a")}
+      ; {fieldName="z";ftyp=TBool} ] in
+    let c = Tctxt.add_struct c "b" [
+        {fieldName="x";ftyp=
+          TRef (RFun ([TNullRef RString], RetVal (TNullRef (RArray TInt))))
+        }
+      ; {fieldName="y";ftyp=TNullRef (RStruct "a")} ] in
     if Typechecker.subtype c (TRef (RStruct "a")) (TRef (RStruct "b"))
     then ()
     else failwith "should not fail") ]
   )
 
 let unit_fail = Test ("gradedtest unit fail",
-  [ "typ_structex fail", (fun () ->
-    let c = Tctxt.add_struct Tctxt.empty "a" [{fieldName="x";ftyp=TInt}] in
-    let c = Tctxt.add_struct c "b" [{fieldName="y";ftyp=TInt}] in
+  [ "no_sub_substruct", (fun () ->
+    let c = Tctxt.add_struct Tctxt.empty "a" [
+        {fieldName="x";ftyp=TInt}
+      ; {fieldName="y";ftyp=TRef (RArray TBool)}
+      ; {fieldName="z";ftyp=TNullRef RString} ] in
+    let c = Tctxt.add_struct c "b" [
+        {fieldName="w";ftyp=TInt}
+      ; {fieldName="z";ftyp=TRef (RArray TBool)}
+      ; {fieldName="x";ftyp=TNullRef RString}
+      ; {fieldName="y";ftyp=TRef (RArray (TRef RString))} ] in
     if Typechecker.subtype c (TRef (RStruct "a")) (TRef (RStruct "b"))
     then failwith "should not succeed"
     else ()) ]
   )
 
+(* the two unit tests *)
 let graded_unit_tests : suite =
   [ unit_ok
   ; unit_fail ]
