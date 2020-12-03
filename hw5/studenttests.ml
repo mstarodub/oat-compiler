366b8824d504937faacbdf39e135fcbe433bdccb
Maxim Starodub <smaxim@student.ethz.ch>
2020-12-03 18:48:30 +0100

    tests: add a couple, some failing

--- hw5/studenttests.ml
+++ hw5/studenttests.ml
@@ -8,14 +8,23 @@ open Assert
 let tc_ok_tests = [
   "test/ok1.oat"
   ; "test/ok2.oat"
+  ; "test/zeroarg.oat"
 ]
 
 let tc_err_tests = [
   "test/fail1.oat"
   ; "test/fail2.oat"
+  ; "test/redeclarelocal.oat"
+  ; "test/minilocalfail1.oat"
+  ; "test/minilocalfail2.oat"
+]
+
+let run_tests = [
+  (* ("test/ifq.oat", "", "Hello") *)
 ]
 
 let provided_tests : suite = [
   Test("student test - tc ok tests", Gradedtests.typecheck_file_error tc_err_tests);
   Test("student test - tc err tests", Gradedtests.typecheck_file_correct tc_ok_tests);
+  Test("student test - executed", Gradedtests.executed_oat_file run_tests);
 ]
